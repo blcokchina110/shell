dba4d0d5fdbace998cbc1e279cf1c93b1b6dac8e
blockchain110 <99483895+blcokchina110@users.noreply.github.com>
2023-09-05 12:14:47 +0800

    Create docker_clean.sh

---
+++ docker_clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "=================== start clean docker containers logs ==========================" 
+ 
+logs=$(find /var/lib/docker/containers/ -name *-json.log)
+ 
+for log in $logs; 
+do
+    echo "clean logs:" 
+    echo $log
+    cat /dev/null>$log
+done
+ 
+echo "==================== end clean docker containers logs =========================="
+ 
+echo `date`
+
